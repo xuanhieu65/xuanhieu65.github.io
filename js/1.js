c79e587967b36f4800330b68958edeec4402e247
xuanhieu65 <xuanhieu0605@gmail.com>
2019-01-22 22:04:48 +0700

    html cho khoi kinh nghiem 2

--- js/1.js
+++ js/1.js
@@ -1,4 +1,12 @@
-﻿ $(function(){
- 
+﻿$(function(){
+	vitricuaedu = $('#khoiedu').offset().top;
+ 	$('.xuong, .kn').click(function(event) {
+ 		$('html').animate({scrollTop: vitricuaedu},700);
+ 		return false;
+ 	});
+ 	$('.sp').click(function(event) {
+ 		$('html').animate({scrollTop: $('#khoisanpham').offset().top},700);
+ 		return false;
+ 	});
 })  
  
